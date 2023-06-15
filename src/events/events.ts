2d4e7542126888aeadd303570ab749326bd812fd
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2023-06-15 12:54:52 -0300

    refactor: socket event handling into separate file

---
+++ src/events/events.ts
@@ -0,0 +1,12 @@
+import { Socket } from 'socket.io';
+import { handleMessage, handleDisconnect } from "../handlers/handlers";
+import { resetTimer } from "../handlers/timer";
+import { Message } from "../interfaces/Message";
+
+export const addSocketEvents = (socket: Socket, timer: any) => {
+    socket.on('message', (content: Message) => {
+      handleMessage(content);
+      timer = resetTimer(socket, timer);
+    });
+    socket.on('disconnect', handleDisconnect(socket));
+  }
--- src/handlers/handlers.ts
+++ src/handlers/handlers.ts
@@ -3,43 +3,38 @@ import UsersList from "../data/UsersList";
 import { resetTimer } from './timer';
 import MessageStorage from '../data/StoredMessages';
 import {emitCurrentlyConnectedIO, emitPreviousMessagesSocket, emmitUpdateMessages} from './emits'
-
 import { Message } from '../interfaces/Message';
 import { ConnectedUser } from '../interfaces/ConnectedUser';
+import { addSocketEvents } from '../events/events';
 
 export const handleConnection = (socket: Socket) => {
-  console.log('Uma conexão foi estabelecida');
-  let timer: any;
-  timer = resetTimer(socket, timer);
   const username = socket.handshake.query.username as string;
   const connectedUser: ConnectedUser = {
     id: socket.id,
     username: username
   };
+  console.log('Uma conexão foi estabelecida');
+  let timer: any;
+  timer = resetTimer(socket, timer);
   UsersList.addNewUser(connectedUser);
   emitPreviousMessagesSocket(socket);
   emitCurrentlyConnectedIO();
-  console.log("usuários conectados", UsersList.getAllConnectedUsers());
-  socket.on('message', (content: Message) => {
-    handleMessage(content);
-    timer = resetTimer(socket, timer);
-  });
-  socket.on('disconnect', handleDisconnect(socket));
+  addSocketEvents(socket, timer);
 };
 
 export const handleMessage = (content: Message) => {
-  console.log(content)
-  MessageStorage.pushNewMessage(content)
-  emmitUpdateMessages()
+  console.log(content);
+  MessageStorage.pushNewMessage(content);
+  emmitUpdateMessages();
 };
 
 export const handleDisconnect = (socket: Socket) => () => {
-  UsersList.removeUserBySocketId(socket.id)
+  UsersList.removeUserBySocketId(socket.id);
   emitCurrentlyConnectedIO();
   console.log('Um usuário saiu.');
   const userLeaving = socket.handshake.query.username as string;
-  const content = { username: "System", message: `${userLeaving} saiu` }
+  const content = { username: "System", message: `${userLeaving} saiu` };
   MessageStorage.pushNewMessage(content);
-  emmitUpdateMessages()
+  emmitUpdateMessages();
   socket.disconnect();
 };
