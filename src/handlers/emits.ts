fc39ad68ca039fd1207b1866731bc53a29509db0
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2023-06-15 11:27:22 -0300

    update: adapting to the new message storage format

--- src/handlers/emits.ts
+++ src/handlers/emits.ts
@@ -1,11 +1,12 @@
-import { inMemoryMessages } from "../data/storedMessages";
+import MessageStorage from "../data/storedMessages";
 import { connectedUsers } from "../data/usersList";
 import { Socket } from "socket.io";
 import { io } from '../server';
 
 
 export const emitPreviousMessagesSocket = (socket: Socket) => {
-    socket.emit('previousMessages', inMemoryMessages);
+    const messages = MessageStorage.getMessages()
+    socket.emit('previousMessages', messages);
   };
   
 export const emitCurrentlyConnectedIO = () => {
@@ -13,5 +14,6 @@ export const emitCurrentlyConnectedIO = () => {
   };
   
   export const emmitUpdateMessages = () => {
-    io.emit('message', inMemoryMessages);
+    const messages = MessageStorage.getMessages()
+    io.emit('message', messages);
   }
