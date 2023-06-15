4f3eecde73998e3dd08eee9b24b4da6eeff62b3d
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2023-06-15 11:17:29 -0300

    changed message storage to a class

--- src/data/storedMessages.ts
+++ src/data/storedMessages.ts
@@ -1,7 +1,15 @@
 import { Message } from "../interfaces/Message";
 
-export let inMemoryMessages: Message[] = [];
-
-export const pushNewMessage = (message: Message) =>{
-    inMemoryMessages.push(message);
+class MessageStorage {
+    messages: Message[];
+    constructor(){
+        this.messages = [];
+    }
+    pushNewMessage = (message: Message) =>{
+        this.messages.push(message);
+    }
+    getMessages = () => {
+        return this.messages
+    }
 }
+export default new MessageStorage;
